07d12346d12e4035a400920487ea256d12d8b6c1
Ilhan Yumer <ilhan@ilhan.name>
2021-01-01 12:32:31 +0300

    API documentation

---
+++ src/main/java/name/ilhan/student/config/SpringFoxConfig.java
@@ -0,0 +1,28 @@
+package name.ilhan.student.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Student API")
+                .build();
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("name.ilhan.student"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+}
