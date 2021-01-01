cc23f56f604b5b184513c82427326fd1c7066240
Ilhan Yumer <ilhan@ilhan.name>
2021-01-01 11:51:31 +0300

    Initial commit

---
+++ src/test/java/name/ilhan/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package name.ilhan.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
